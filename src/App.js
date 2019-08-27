92de85e74634a4616ae02e673d9d0e0b53385637
Chiheb Hmida <chiheb.hmida@gmail.com>
2019-08-27 19:35:40 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Parent from './components/Parent';
+
+
+const App = () => {
+
+
+
+ return (
+          <div className='container mt-5'>
+
+        
+
+        
+     <Parent />
+
+        
+            
+          </div>
+      );
+ 
+};
+export default App;
